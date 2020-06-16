27b8450fc55ef2d6acad9517f571021eb5860f80
Freddy <furede87@gmail.com>
2020-06-16 11:02:21 -0600

    Comment out UpdatesLog

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/App.css";
 import Gacha from "./Gacha";
-import UpdatesLog from "./UpdatesLog";
+//import UpdatesLog from "./UpdatesLog";
 
 class App extends React.Component {
   render() {
