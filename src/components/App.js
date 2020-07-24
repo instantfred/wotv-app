1d2d0ee73c3daee8a9f6ad3adb6771e22eb9e850
Freddy <furede87@gmail.com>
2020-07-23 20:59:33 -0600

    Add ko-fi donations button

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,15 @@ class App extends React.Component {
       <div className="app">
         <nav className="navbar">
           <div className="nav-title">Summon Simulator</div>
+          <div className="donation">
+            <a href="https://ko-fi.com/F1F51J6VY" target="_blank">
+              <img
+                className="kofi-img"
+                src="https://cdn.ko-fi.com/cdn/kofi1.png?v=2"
+                alt="Buy Me a Coffee at ko-fi.com"
+              />
+            </a>
+          </div>
         </nav>
         <Gacha gachaParentCallBack={this.gachaCallBack} />
         <GachaLog urItems={this.state.luckyPulls} />
